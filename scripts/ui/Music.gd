6c0231d94f738774484517643826738dff3c1d11
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-12-08 13:28:22 -0500

    Add separate audio buses for sound effects and music.

--- scripts/ui/Music.gd
+++ scripts/ui/Music.gd
@@ -6,6 +6,7 @@ export var audio_bus_name := "Master"
 # The amount to change the volume by
 export var amount = 0.25
 
+onready var label = get_node("Label")
 onready var sfx = get_node("SFX")
 onready var gauge = get_node("Gauge")
 onready var bus := AudioServer.get_bus_index(audio_bus_name)
@@ -13,6 +14,7 @@ onready var bus := AudioServer.get_bus_index(audio_bus_name)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	gauge.value = db2linear(AudioServer.get_bus_volume_db(bus))
+	label.text = audio_bus_name
 
 func _on_Up_pressed():
 	gauge.value = gauge.value + amount
